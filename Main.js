dbdc816b9d4e8260ec603bab882e68a34a2e3bf5
Azuani <48812825+Azuani@users.noreply.github.com>
2019-04-23 05:48:18 -0500

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var imgItems = $('.slider li').length;
+
+	for(i=1; i<=imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	}
+
+	$('.slider li').hide();
+	$('.slider li:first').show();
+	$('.pagination li:first').css({'color': '#CD6E2E'});
+
+});
